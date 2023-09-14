5c0e4850d3750f5aaaa097c7ee93506b4f3b6abc
Joao Victor Viana Marques <vjoaovictor746@gmail.com>
2023-09-14 16:48:02 -0300

    oo

---
+++ Objeto/oo.js
@@ -0,0 +1,16 @@
+//CÓDIGO NÃO EXECUTÁVEL
+
+//PROCEDUAL
+processamento (4, 10, 7)
+
+//O
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //...
+    }
+}
+objeto.processamento() //foco passou a ser o objeto
+  //pontos importantes
