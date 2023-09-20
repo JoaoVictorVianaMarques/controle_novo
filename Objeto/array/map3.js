47f04aa5104c39e961e6cc5e5781bdc235f8d904
Joao Victor Viana Marques <vjoaovictor746@gmail.com>
2023-09-20 13:55:33 -0300

    map3

---
+++ Objeto/array/map3.js
@@ -0,0 +1,23 @@
+Array.prototype.map2 = function(callback){
+    const newArray = []
+    for (let i = 0; i < this.length; i++) {
+        newArray.push(callback(this[i], i, this))
+    }
+    return newArray
+}
+
+
+const carrinho = [
+    '{ "nome": "Borracha", "preco": 3.45 }',
+    '{ "nome": "Caderno", "preco": 14.45 }',
+    '{ "nome": "Kit de lapís", "preco": 41.45 }',
+    '{ "nome": "Caneta", "preco": 6.45 }'
+
+]
+
+//Retornar um array apenas com os preços
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco
+
+const resultado = carrinho.map2(paraObjeto).map2(apenasPreco)
+console.log(resultado)
