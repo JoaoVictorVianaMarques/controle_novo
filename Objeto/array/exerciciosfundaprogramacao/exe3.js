93462f1c01a86d98f1ff412510e7382d0df4db15
Joao Victor Viana Marques <vjoaovictor746@gmail.com>
2023-09-22 13:55:59 -0300

    exe4

--- Objeto/array/exerciciosfundaprogramacao/exe3.js
+++ Objeto/array/exerciciosfundaprogramacao/exe3.js
@@ -2,10 +2,10 @@
 //um é a quantidade de horas trabalhadas por um funcionário num mês, e o quanto ele recebe por hora.
 //O retorno da função deve ser a string "Salário igual a R$ X", em que X é o  quanto o funcionário
 //ganhou no mês. 
-function calcularSalarioLiquido(horasTrabalhadas, ganhoPorHora) {
+function calcularSalario(horasTrabalhadas, ganhoPorHora) {
     const salarioBruto = horasTrabalhadas * ganhoPorHora
     const salarioLiquido = salarioBruto - salarioBruto * 30/100
     return `Salário igual a R$ ${salarioLiquido}`
 }
 
-console.log(calcularSalarioLiquido(25, 150))
+console.log(calcularSalario(25, 150))
