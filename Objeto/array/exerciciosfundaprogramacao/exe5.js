43f119241b0c5bcc3d8377cf92ccde4d1c2eb79e
Joao Victor Viana Marques <vjoaovictor746@gmail.com>
2023-09-22 14:43:16 -0300

    exe5

---
+++ Objeto/array/exerciciosfundaprogramacao/exe5.js
@@ -0,0 +1,14 @@
+//Crie uma função que receba dois números e retorne se o primeiro é maior ou igual ao segundo.
+
+
+function maiorOuIgual(primeiro, segundo){
+  if(typeof primeiro != typeof segundo ) return false
+    return primeiro >= segundo
+
+}
+console.log(Math.max(10, -20))
+console.log(Math.max(-22, 22))
+console.log(Math.max(10, -1))
+
+
+
