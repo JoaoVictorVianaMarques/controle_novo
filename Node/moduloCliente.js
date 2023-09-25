549f153426fb9bd3f03457ac417e04a3df5f27d0
Joao Victor Viana Marques <vjoaovictor746@gmail.com>
2023-09-25 15:45:17 -0300

    modulo

---
+++ Node/moduloCliente.js
@@ -0,0 +1,7 @@
+const moduloA = require('./moduloA')
+const moduloB =  require('./muduloB')
+
+console.log(moduloA.ola)
+console.log(moduloA.bemVindo)
+console.log(moduloA.atelogo)
+console.log(moduloA)
