37d9ec56eaf72aaa1facd0ba66458e06b0badc92
Joao Victor Viana Marques <vjoaovictor746@gmail.com>
2023-09-26 13:20:57 -0300

    modulo terceiros

--- Node/moduloCliente.js
+++ Node/moduloCliente.js
@@ -5,3 +5,8 @@ console.log(moduloA.ola)
 console.log(moduloA.bemVindo)
 console.log(moduloA.atelogo)
 console.log(moduloA)
+
+console.log(moduloB.ola)
+console.log(moduloB.bemVindo)
+console.log(moduloB.atelogo)
+console.log(moduloB)
