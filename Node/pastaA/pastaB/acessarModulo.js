0625630e42c52a4543442f3f0e9b496e65d07aac
Joao Victor Viana Marques <vjoaovictor746@gmail.com>
2023-09-27 13:38:18 -0300

    acessar

--- Node/pastaA/pastaB/acessarModulo.js
+++ Node/pastaA/pastaB/acessarModulo.js
@@ -4,8 +4,12 @@ console.log(moduloA.ola)
 const saudacao = require('saudacao')
 console.log(saudacao.ola)
 
-const http = require('http')
-http.createServer((req, res) => {
-    res.write('Bom dia!')
-    res.end()
-}).listen(8080)
+const c = require('./pastaC/index')
+console.log(c.ola2)
+
+
+//const http = require('http')
+//http.createServer((req, res) => {
+  //  res.write('Bom dia!')
+    //res.end()
+//}).listen(8080)
