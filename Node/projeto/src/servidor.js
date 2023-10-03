586796ba072b8c1be205208deabfd2af93036e6d
Joao Victor Viana Marques <vjoaovictor746@gmail.com>
2023-10-03 16:13:48 -0300

    postman

--- Node/projeto/src/servidor.js
+++ Node/projeto/src/servidor.js
@@ -2,12 +2,38 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({ extended: true}))
+
 
 app.get('/produtos', (req, res, next) => {
-    res.send({ nome: 'Notebook', preco: 129.99 }) //converter para JSON
+    res.send(bancoDeDados.getProdutos())
 
 })
 
+app.get('/produtos/:id', (req, res, next) => {  
+    res.send(bancoDeDados.getProdutos(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+  const produto = bancoDeDados.salvarProduto({
+     nome: req.body.nome,
+     preco: req.body.preco
+    })
+    res.send(produto) // JSON
+
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+       nome: req.body.nome,
+       preco: req.body.preco
+      })
+      res.send(produto) // JSON
+  
+  })
 app.listen(porta, () => {
     console.log(`Servidor está executando na porta ${porta}.`)
 })
